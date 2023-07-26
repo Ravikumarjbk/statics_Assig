e7609bf7f72895f862b9f9bdaf48f3de0ace926f
Ravikumarjbk <90333417+Ravikumarjbk@users.noreply.github.com>
2023-07-26 17:49:54 +0530

    Add files via upload

---
+++ static_ds.py
@@ -0,0 +1,108 @@
+#Q1
+
+"""" Statistics is a branch of mathematics that involves the collection, analysis,
+interpretation, presentation, and organization of data. It is a crucial field of study used to make informed decisions,
+draw conclusions, and make predictions based on data from various sources. Statistics
+ plays a significant role in scientific research, social sciences, business, finance, economics, medicine, engineering,
+  and many other fields."""
+#Q2
+""""
+1> descriptive statistic:  Suppose you have data on the heights of students in a class. 
+Descriptive statistics would be used to calculate the mean height, median height, 
+and standard deviation of the heights. 
+This information will provide a summary of the central tendency and variability of the students' heights in the class.
+
+2> inferential statistics:  Suppose you want to know the average income of all working adults in a country.
+ It is impractical to collect data from the entire population, 
+ so you take a random sample of working adults and calculate their average income. 
+ Inferential statistics will be used to estimate the average income of the entire working population based on the sample data."""
+#Q3
+# (i) Grading in exam: A+, A, B+, B, C+, C, D, E:  Qualitative data type
+# (ii) Colour of mangoes: yellow, green, orange, red: Qualitative data type
+# (iii) Height data of a class: [178.9, 179, 179.5, 176, 177.2, 178.3, 175.8,...]: quantitative data type
+# (iv) Number of mangoes exported by a farm: [500, 600, 478, 672, ...]: quantitative data type
+
+#Q4>""" 1>Nominal level of measurement: At the nominal level, data are categorized into distinct and non-numeric categories or labels.
+"""" EX: Eye colors of a group of people (blue, brown, green)
+2> Ordinal Level of Measurement:
+The ordinal level of measurement involves data that can be ranked or ordered. 
+The categories have a meaningful order, but the intervals between values may not be equal, and the data lacks precise numerical meaning.
+
+Example: Education levels (elementary, high school, bachelor's, master's, Ph.D.).
+3> Interval Level of Measurement:
+Interval data have equal intervals between values, but there is no true zero point. 
+The zero value is arbitrary, and only the differences between values are meaningful. Interval data can be added, subtracted, 
+and used for relative comparisons.
+
+Example: Temperature measured in Celsius or Fahrenheit. While the differences between 10°C and 20°C are equal to the differences between 30°C and 40°C, 0°C does not represent the complete absence of temperature.
+4> Ratio Level of Measurement:
+The ratio level of measurement is similar to the interval level but with a true zero point. 
+Ratios between values are meaningful, and all arithmetic operations (addition, subtraction, multiplication, division) 
+are valid. Ratio data is the most precise level of measurement.
+
+Example: Height, weight, age, time, or income. Ratios between these values are meaningful, and 0 height (no height) or 0 weight (no weight) represents an absence of the attribute."""
+#Q6
+""""The favorite colors in this dataset represent nominal data since they are categories with no inherent order or numerical significance.
+ If someone attempted to perform numerical operations like calculating the mean or median of the favorite colors, 
+ it would be nonsensical because the colors are not numeric values. 
+ In this case, using numerical measures would not provide any meaningful insights."""
+#Q7
+""""1 Nominal Data Example:
+Suppose we have a dataset of different eye colors of people, categorized as "Blue," "Brown," "Green," and "Hazel." 
+These categories are distinct, and there is no inherent order or ranking among them. The eye colors are nominal data.
+
+2 Ordinal Data Example:
+Now, consider a dataset that represents educational levels of individuals, categorized as 
+"Elementary," "High School," "Bachelor's," "Master's," and "Ph.D."""
+#Q8
+"""A box plot, also known as a box-and-whisker plot, is a type of plot that can be used to display data in terms of range"""
+#Q9
+""""
+1> descriptive statistic:  Suppose you have data on the heights of students in a class. 
+Descriptive statistics would be used to calculate the mean height, median height, 
+and standard deviation of the heights. 
+This information will provide a summary of the central tendency and variability of the students' heights in the class.
+
+2> inferential statistics:  Suppose you want to know the average income of all working adults in a country.
+ It is impractical to collect data from the entire population, 
+ so you take a random sample of working adults and calculate their average income. 
+ Inferential statistics will be used to estimate the average income of the entire working population based on the sample data."""
+#10
+""" 1 Mean: The mean is the most common measure of central tendency. 
+It is the sum of all data values divided by the total number of data points. The mean represents the average value of the dataset.
+
+How it can be used: The mean is used to describe the typical value or average value of the data. 
+It is sensitive to extreme values (outliers) and may not be the best representation of the central tendency when the data contains outliers.
+
+2 Median: The median is the middle value of the dataset when the data is arranged in ascending order. 
+If the dataset has an even number of values, the median is the average of the two middle values.
+
+How it can be used: The median is robust to extreme values and outliers and provides a better representation of the central tendency when the data contains such values.
+ It is often used when the data is skewed or has outliers.
+
+3 Mode: The mode is the value that appears most frequently in the dataset.
+
+How it can be used: The mode is useful for describing the most common value or category in the dataset. 
+It is particularly valuable for categorical data, such as favorite colors or customer preferences.
+
+Measures of Variability:
+
+Range: The range is the difference between the maximum and minimum values in the dataset.
+ It provides a simple measure of the spread of the data.
+
+How it can be used: The range gives an idea of how much the data is spread out. However, 
+it is sensitive to extreme values and may not be representative of the overall variability when the dataset contains outliers.
+
+Variance: The variance measures the average squared deviation of each data point from the mean.
+ It quantifies the spread of the data points around the mean.
+
+How it can be used: Variance provides a more precise measure of the spread compared to the range. However,
+ it is in squared units, which can make interpretation challenging. Taking the square root of the variance gives us the standard deviation.
+
+Standard Deviation: The standard deviation is the square root of the variance. It measures the average deviation of each data point from the mean.
+
+How it can be used: The standard deviation is widely used in statistics because it is in the same units as the data and 
+provides a measure of the typical distance between data points and the mean. It is helpful in understanding the dispersion or variability of thedata.
+
+These measures of central tendency and variability are fundamental in descriptive statistics and are used to provide a comprehensive summary of a dataset. Depending on the characteristics of the data and the research question, different measures may be more appropriate for describing the data's distribution and characteristics.
+"""
